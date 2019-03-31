a93ae8a8cc9f5d13168f6590c02f26daf3906290
Cícero Castro <cicerobcastro@gmail.com>
2019-03-31 18:51:51 -0300

    Add files via upload

--- src/component/ListRestaurants.js
+++ src/component/ListRestaurants.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
+import ListGroup from 'react-bootstrap/ListGroup'
+import { slide as Menu } from 'react-burger-menu';
 
-function ListRestaurants(props) {
-    console.log('Props', props)
-    return (
-        <ol className='restaurantsList'>
-             {props.venues.map((venue) => (
-                    <li key={venue.venue.id} >
-                        {venue.name}
-                    </li>
-                ))} 
-        </ol>
-    )
+class ListRestaurants extends Component {
+
+    /*
+    When restaurants are selected in ListRestaurants.
+    Will open into the map.
+    */
+    openMarker = locationName => {
+        this.props.markers.map(marker => {
+            if (marker.title === locationName) {
+                window.google.maps.event.trigger(marker, "click")
+                marker.setIcon('https://maps.google.com/mapfiles/ms/icons/blue-dot.png')
+            } else {
+                marker.setIcon('https://maps.google.com/mapfiles/ms/icons/red-dot.png')
+            }
+        })
+    }
+
+    render() {
+        return (
+            /*My list of Restaurants*/
+            <ListGroup>
+                <ul className="list-group" >
+                    {this.props.venues.map((myVenue) => (
+                        <li role="menuitem"
+                            onClick={() => this.openMarker(myVenue.venue.name)}
+                            className="list-group-item"
+                            tabIndex="0"
+                            key={myVenue.venue.id}
+                            id={myVenue.venue.id}
+                            aria-label={myVenue.venue.name}
+                        >
+                            <a>{myVenue.venue.name}</a>
+                        </li>))}
+                </ul>
+            </ListGroup>
+        );
+    }
+
 }
 
 export default ListRestaurants
---
+++ src/component/NavBar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <p className="navbar-brand">{this.props.logo}</p>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <form className="form-inline my-2 my-lg-0 search">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={this.props.query} onChange={(event) => this.props.updateQuery(event.target.value)} />
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
