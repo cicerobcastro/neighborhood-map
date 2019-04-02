e0dc5073c330c3f27049bad6e7e2b82505d2c48b
Cícero Castro <cicerobcastro@gmail.com>
2019-04-01 22:46:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-//import NavBar from './ui/NavBar';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
