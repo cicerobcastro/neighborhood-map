738aebf60b8fd9e692dfeec4a0022b518e7f8524
Cícero Castro <cicerobcastro@gmail.com>
2019-04-01 22:40:52 -0300

    Update NavBar.js

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-/* This component is the SearchBar with "Logo" */
 class NavBar extends Component {
 
     render() {
@@ -12,8 +11,8 @@ class NavBar extends Component {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <form className="form-inline my-2 my-lg-0 search">
-                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={this.props.query} onChange={(event) => this.props.updateQuery(event.target.value)} />
+                        <form className="form-inline my-2 my-lg-0">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={this.props.query} onChange={(event) => this.props.attQuery(event.target.value)} />
                         </form>
                     </div>
                 </nav>
@@ -22,4 +21,4 @@ class NavBar extends Component {
     }
 }
 
-export default NavBar;
+export default NavBar
